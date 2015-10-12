a94313082d524437ce031c2163640b2b5d862ef2
Leif Andersen <leif@leifandersen.net>
2015-10-12 18:44:33 -0400

    Can now have source lines in R blocks.

--- main.rkt
+++ main.rkt
@@ -1,11 +1,21 @@
 #lang racket/base
 
-(require scribble/core scribble/manual)
+(require scribble/core scribble/manual
+         (for-syntax racket/base
+                     syntax/parse))
 
 (provide (all-defined-out))
 
 ;; from Ben Lerner
-(define-syntax-rule (R-block arg args ...)
-  (codeblock #:keep-lang-line? #f #:context #'arg "#lang r-lexer\n" arg args ...))
+(define-syntax (R-block stx)
+  (syntax-parse stx
+    [(_ (~or (~optional (~seq #:indent indent) #:defaults ([indent #'1]))
+             (~optional (~seq #:line-numbers line-numbers) #:defaults ([line-numbers #'#f]))
+             (~optional (~seq #:line-number-sep line-number-sep) #:defaults ([line-number-sep #'1])))
+        ...
+        arg args ...)
+     #'(codeblock #:indent indent #:line-numbers line-numbers #:line-number-sep line-number-sep
+                  #:keep-lang-line? #f #:context #'arg
+                "#lang r-lexer\n" arg args ...)]))
 (define-syntax-rule (R arg args ...)
   (make-element (make-style "RktBlk" '(tt-chars)) (code #:lang "r-lexer" arg args ...)))
